9d47331b7188191ec59f325a7096f59d9a146c47
Eli <edm.denovosoft@gmail.com>
2022-04-03 11:25:51 +0100

    Add player eyes.

--- src/entity/player.js
+++ src/entity/player.js
@@ -23,9 +23,12 @@ function Player(x,y) {
   this.nearby = [];
   
   this.bobTime = 0;
+  this.blinkTime = 0;
+  this.blinkAmount = 0;
 }
 
 Player.DRAW_RADIUS = 0.4;
+Player.GLOW_RADIUS = 0.6;
 Player.MAX_SPEED = 0.007;
 Player.ACCELERATION_START = 0.003;
 Player.ACCELERATION_STOP = 0.01;
@@ -34,10 +37,20 @@ Player.ANGLE_CHANGE = Math.PI * 0.002;
 Player.BOB_PERIOD = 2000;
 Player.BOB_OFFSET = 0.2;
 Player.BOB_SPEED_FACTOR = 1.5;
+
 Player.SHADOW_OFFSET = 0.5;
 Player.SHADOW_BOB_AMOUNT = 0.2;
 Player.SHADOW_WIDTH = 2 * Player.DRAW_RADIUS - Player.SHADOW_BOB_AMOUNT;
 Player.SHADOW_HEIGHT = Player.SHADOW_WIDTH / 2;
+
+Player.EYE_OFFSET_X = 0.125;
+Player.EYE_WIDTH = 0.1;
+Player.EYE_HEIGHT = 0.4;
+Player.EYE_VELOCITY_FACTOR = 0.2;
+
+Player.BLINK_PERIOD = 2000;
+Player.BLINK_LENGTH = 400;
+Player.BLINK_HURT = 150;
 
 Player.prototype.update = function(elapsed, world) {
   this.targetVelocity.x = 0;
@@ -111,12 +124,28 @@ Player.prototype.update = function(elapsed, world) {
   
   this.bounds.moveTo(this.predictedBounds.left, this.predictedBounds.top);
   
-  this.animationUpdate(elapsed);
+  this.animationUpdate(elapsed, world);
 };
-Player.prototype.animationUpdate = function (elapsed) {
+Player.prototype.animationUpdate = function (elapsed, world) {
   var vm = Math.sqrt(this.velocity.x*this.velocity.x+this.velocity.y*this.velocity.y);
   
   this.bobTime += elapsed * (1 + vm * Player.BOB_SPEED_FACTOR);
+  
+  if (world.moldTimer > 0) {
+    this.blinkTime = Player.BLINK_PERIOD;
+  }
+  
+  if (this.blinkTime < Player.BLINK_PERIOD) {
+    this.blinkTime += elapsed;
+  } else if (this.blinkAmount + elapsed < Player.BLINK_LENGTH) {
+    this.blinkAmount += elapsed;
+    if (world.moldTimer > 0 && this.blinkAmount > Player.BLINK_HURT) {
+      this.blinkAmount = Player.BLINK_HURT;
+    }
+  } else {
+    this.blinkTime -= Player.BLINK_PERIOD;
+    this.blinkAmount = 0;
+  }
 };
 Player.prototype.draw = function(ctx, camera) {
   var bobPosition = Math.sin(this.bobTime * 2 * Math.PI / Player.BOB_PERIOD);
@@ -133,15 +162,59 @@ Player.prototype.draw = function(ctx, camera) {
   ctx.fill();
   
   // Draw character
+  var bobOffset = bobPosition * Player.BOB_OFFSET - Player.SHADOW_OFFSET;
+  
   this.drawBounds.copyFrom(this.bounds);
   this.drawBounds.resizeCentered(1,1);
-  this.drawBounds.moveBy(0, bobPosition * Player.BOB_OFFSET - Player.SHADOW_OFFSET);
+  this.drawBounds.moveBy(0, bobOffset);
   this.drawBounds = camera.screenRect(this.drawBounds, this.drawBounds);
   
-  ctx.fillStyle = "#ffffff";
+  // ctx.fillStyle = "#ffffff";
+  // ctx.beginPath();
+  // ctx.arc(this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width * Player.DRAW_RADIUS, 0, 2*Math.PI);
+  // ctx.fill();
+  
+  var glow = ctx.createRadialGradient(
+    this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width * Player.DRAW_RADIUS, 
+    this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width * Player.GLOW_RADIUS);
+  glow.addColorStop(0, 'white');
+  glow.addColorStop(0, 'rgba(255,255,255,0.5)');
+  glow.addColorStop(1, 'transparent');
+    
+  ctx.fillStyle = glow;
   ctx.beginPath();
-  ctx.arc(this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width * Player.DRAW_RADIUS, 0, 2*Math.PI);
+  ctx.arc(this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width * Player.GLOW_RADIUS, 0, 2*Math.PI);
   ctx.fill();
+  
+  // Draw face
+  var ex = this.velocity.x * Player.EYE_VELOCITY_FACTOR;
+  var ey = this.velocity.y * Player.EYE_VELOCITY_FACTOR + bobOffset;
+  var blink = 1;
+  
+  if (this.blinkAmount > 0) {
+    blink = Math.cos(this.blinkAmount * 2 * Math.PI / Player.BLINK_LENGTH) / 2 + 0.5;
+  }
+  
+  this.drawBounds.copyFrom(this.bounds);
+  this.drawBounds.resizeCentered(Player.EYE_WIDTH,Player.EYE_HEIGHT * blink);
+  this.drawBounds.moveBy(ex-Player.EYE_OFFSET_X, ey);
+  this.drawBounds = camera.screenRect(this.drawBounds, this.drawBounds);
+  
+  ctx.fillStyle = "#000000";
+  ctx.beginPath();
+  ctx.ellipse(this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width / 2, this.drawBounds.height / 2, 0, 0, 2*Math.PI);
+  ctx.fill();
+  
+  this.drawBounds.copyFrom(this.bounds);
+  this.drawBounds.resizeCentered(Player.EYE_WIDTH,Player.EYE_HEIGHT * blink);
+  this.drawBounds.moveBy(ex+Player.EYE_OFFSET_X, ey);
+  this.drawBounds = camera.screenRect(this.drawBounds, this.drawBounds);
+  
+  ctx.fillStyle = "#000000";
+  ctx.beginPath();
+  ctx.ellipse(this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width / 2, this.drawBounds.height / 2, 0, 0, 2*Math.PI);
+  ctx.fill();
+  
   
   // ctx.beginPath();
   // ctx.arc(this.drawBounds.centerX, this.drawBounds.centerY, this.drawBounds.width * this.bounds.radius, 0, 2*Math.PI);
